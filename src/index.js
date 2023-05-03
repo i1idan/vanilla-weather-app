d661aeea30e51b7734415910984c53762fb45a64
maryam saeedi <maryam.saeedi33@gmail.com>
2023-05-04 00:02:48 +0330

    add forecast

--- src/index.js
+++ src/index.js
@@ -9,8 +9,58 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
   }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-let apiKey = "cb66bf594bf675942f1ebbf913157ef9";
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector(".weather-forecast");
+
+
+let forecastHTML = `<div class="row weather-forecast-row">`;
+forecast.forEach(function (forecastDay, index) {
+  if (index < 5) {
+    forecastHTML =
+      forecastHTML +
+      `
+    <div class="col-2">
+      <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+      <img
+        style="filter:hue-rotate(280deg);" src="http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png"
+        alt=""
+        width="62"
+        
+      />
+      <div class="weather-forecast-temperatures">
+        <span class="weather-forecast-temperature-max"> ${Math.round(
+          forecastDay.temp.max
+        )}° </span>
+        <span class="weather-forecast-temperature-min"> ${Math.round(
+          forecastDay.temp.min
+        )}° </span>
+      </div>
+    </div>
+  `;
+  }
+});
+forecastHTML = forecastHTML + `</div>`;
+forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "bd5b4461863eddaa6ced0a0a67989e0a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 
 function manipulation(response) {
   let cityHolder = document.querySelector(".cityName");
@@ -36,6 +86,7 @@ function manipulation(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconHolder.setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
 
 }
 
@@ -76,6 +127,7 @@ let currentElement = document.querySelector(".currentLocation");
 // currentElement.addEventListener("click", getCurrent);
 
 let city = "tehran";
+let apiKey = "bd5b4461863eddaa6ced0a0a67989e0a";
 let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 axios.get(url).then(defaultCity);
 
